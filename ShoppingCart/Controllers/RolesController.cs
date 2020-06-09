f5f35374440a0650ac483256652a301791e8cfe4
Sunil Kumar Sahoo <sahoosunil250@gmail.com>
2020-06-09 23:25:43 +0530

    Add Styles

---
+++ ShoppingCart/Controllers/RolesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class RolesController : Controller
+    {
+        private ShoppingContext db = new ShoppingContext();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var roleTable = db.RoleTable.Include(r => r.Users);
+            return View(roleTable.ToList());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.RoleTable.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.UserTable, "UserId", "UserName");
+            return View();
+        }
+
+        // POST: Roles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RoleId,RoleName,UserId")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RoleTable.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.UserTable, "UserId", "UserName", role.UserId);
+            return View(role);
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.RoleTable.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.UserTable, "UserId", "UserName", role.UserId);
+            return View(role);
+        }
+
+        // POST: Roles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RoleId,RoleName,UserId")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.UserTable, "UserId", "UserName", role.UserId);
+            return View(role);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.RoleTable.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Role role = db.RoleTable.Find(id);
+            db.RoleTable.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ShoppingCart/Models/RoleUserMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.Models
+{
+    public class RoleUserMap
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public  string RoleName { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
